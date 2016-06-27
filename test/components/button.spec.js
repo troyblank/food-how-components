8b2e54553bd622bee7dca1a62cbb417733e8c376
Troy Blank <troy@troyblank.com>
2016-06-26 22:38:59 -0500

    passing list data to shoppingList component

--- test/components/button.spec.js
+++ test/components/button.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../helpers/style-fakes';
 import Chance from 'chance';
 import { assert } from 'chai';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 import { Button } from '../../components/index';
 
 describe('Button', () => {
@@ -10,21 +10,21 @@ describe('Button', () => {
     const buttonText = chance.word();
 
     it('should render a button', () => {
-        const wrapper = render(<Button />);
+        const wrapper = shallow(<Button />);
         const button = wrapper.find('button');
 
         assert.equal(button.length, 1);
     });
 
     it('should apply styles', () => {
-        const wrapper = render(<Button />);
+        const wrapper = shallow(<Button />);
         const button = wrapper.find('button');
 
         assert.isTrue(button.hasClass(styles.button));
     });
 
     it('should render text', () => {
-        const wrapper = render(<Button text={buttonText} />);
+        const wrapper = shallow(<Button text={buttonText} />);
         const button = wrapper.find('button');
 
         assert.equal(button.text(), buttonText);
