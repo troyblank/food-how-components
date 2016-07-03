52488dd6c3dfdafc562343639d783cd3420371ad
Troy Blank <troy@troyblank.com>
2016-07-03 11:39:18 -0500

    added no result component

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export Button from './button/button';
 export GetShoppingList from './shoppingList/getShoppingList';
 export GotShoppingList from './shoppingList/gotShoppingList';
+export NoResultMessage from './noResultMessage/noResultMessage';
---
+++ test/components/noResultMessage.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '../helpers/style-fakes';
+import Chance from 'chance';
+import { assert } from 'chai';
+import { shallow } from 'enzyme';
+import { NoResultMessage } from '../../components/index';
+
+describe('No Result Message', () => {
+    const chance = new Chance();
+    const headline = chance.word();
+    const message = chance.word();
+
+    it('should render a shopping list', () => {
+        const wrapper = shallow(<NoResultMessage headline={headline} message={message} />);
+        const divWrapper = wrapper.find('div');
+        const header = wrapper.find('h1');
+        const paragraph = wrapper.find('p');
+
+        assert.isTrue(divWrapper.hasClass(styles['no-result-message']));
+        assert.equal(header.text(), headline);
+        assert.equal(paragraph.text(), message);
+    });
+});
