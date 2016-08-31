5d189f36961b9f2641e24f54108d2102511f0eaa
Troy Blank <troy@troyblank.com>
2016-08-31 16:01:42 -0500

    added base input component

--- test/helpers/mocha-setup.js
+++ test/helpers/mocha-setup.js
@@ -1,4 +1,5 @@
 import styles from './style-fakes';
+import { jsdom } from 'jsdom';
 
 function css(module) {
     const fake = module;
@@ -9,3 +10,17 @@ function css(module) {
 }
 
 require.extensions['.scss'] = css;
+
+// for use with mounted unit tests
+const doc = jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+Object.keys(win).forEach((k) => {
+    if (!win.hasOwnProperty(k)) return;
+    if (k in global) return;
+
+    global[k] = window[k];
+});
